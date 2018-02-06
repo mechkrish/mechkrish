5362b68ee7056f62a3fd91b78c2144427ce1e835
mechkrish <35727408+mechkrish@users.noreply.github.com>
2018-02-06 16:27:48 +0530

    Create vowelll.C

---
+++ vowelll.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    char k;
+    k=getchar();
+    
+    if(k=='a'|k=='e'|k=='i'|k=='o'|k=='u'){
+        printf("Vowel");
+    }else {
+        printf("Consonent");
+   }
