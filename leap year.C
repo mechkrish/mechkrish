3d88944e6e326baf7db36433cd3b21da049a2f77
mechkrish <35727408+mechkrish@users.noreply.github.com>
2018-02-06 16:48:16 +0530

    Create leap year.C

---
+++ leap year.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int y;
+    printf("enter the leep year");
+    scanf("%d",&y);
+if(y%4==0)
+{
+    printf("the leep year");
+}
+else
+{
+    printf(" not leep year");
+    
+}
+}
