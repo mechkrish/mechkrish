39916bcdd50c86603be87a9f95e0eeb0d0acf3e9
mechkrish <35727408+mechkrish@users.noreply.github.com>
2018-02-06 16:42:37 +0530

    Create three num.C

---
+++ three num.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int k,b,c,x;
+    int largest;
+    printf("entre the number");
+    scanf("%d%d%d",&k,&b,&c);
+if(k>b&&k>c)
+printf("largest k");
+else if(b>k)
+printf("largest b");
+else(c>k);
+printf("the largest c");
+  }
