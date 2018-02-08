d18713d40662d1adc305e50b862ebb9740625c34
mechkrish <35727408+mechkrish@users.noreply.github.com>
2018-02-08 15:49:14 +0530

    Create HELLO.C

---
+++ HELLO.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int counter b ;
+    for(counter = 0; counter < b; counter++)
+    {
+      scanf("%d",&b);
+      printf("Hello \b",b);
+    }
+    return 0;
+}
