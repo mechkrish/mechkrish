8906ed5539eceb30e80d149bc2af9e315330c079
mechkrish <35727408+mechkrish@users.noreply.github.com>
2018-02-08 16:22:39 +0530

    Update power of num.C

--- power of num.C
+++ power of num.C
@@ -1,16 +1,16 @@
-#include<stdio.h>
+ff#include<stdio.h>
 int main()
 {
-  int pow,num,j=1;
+  int pow,num,i=1;
   long int sum=1;
   printf("\nEnter a number: ");
   scanf("%d",&num);
   printf("\nEnter power: ");
   scanf("%d",&pow);
-  while(i<=pow)
+  while(j<=pow)
   {
             sum=sum*num;
-            j++;
+            i++;
   }
   printf("\n%d to the power %d is: %ld",num,pow,sum);
   return 0;
